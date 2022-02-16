6cab6ab01a155f04861ce89b0f87563890641128
YPP <3431505558@qq.com>
2022-02-16 14:30:23 +0800

    剑指 Offer 25. 合并两个排序的链表

---
+++ src/main/java/com/jack/review/leetcode/DoublePointer/GetIntersectionNode.java
@@ -0,0 +1,53 @@
+package com.jack.review.leetcode.DoublePointer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Description: 剑指 Offer 52. 两个链表的第一个公共节点
+ * @Auther: Jack You
+ * @Date: 2022/02/15/9:45
+ */
+public class GetIntersectionNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(2);
+        l2.next.next = new ListNode(4);
+
+        ListNode res = new GetIntersectionNode().getIntersectionNode(l1, l2);
+        System.out.println(res);
+    }
+}
