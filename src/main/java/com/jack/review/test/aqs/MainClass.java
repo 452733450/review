63c5207c45725449ceb9655121947afcb2fef2d8
Jack <452733450@qq.com>
2022-03-13 22:07:03 +0800

    整理

---
+++ src/main/java/com/jack/review/test/aqs/MainClass.java
@@ -0,0 +1,12 @@
+package com.jack.review.test.aqs;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ReentrantLock reentrantLock = new ReentrantLock();
+        reentrantLock.lock();
+        reentrantLock.unlock();
+    }
+}
