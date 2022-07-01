77c19321fa721122025660784e48a6b5fa11a036
YPP <3431505558@qq.com>
2022-07-01 17:59:08 +0800

    整理-outside-0701

---
+++ src/main/java/com/jack/asset/Test20220701.java
@@ -0,0 +1,13 @@
+package com.jack.asset;
+
+/**
+ * @Description:
+ * @Auther: Jack You
+ * @Date: 2022/07/01/10:03
+ */
+public interface Test20220701 {
+
+    static final String a = "a";
+
+    void test();
+}
---
+++ src/main/java/com/jack/asset/Test20220701a.java
@@ -0,0 +1,13 @@
+package com.jack.asset;
+
+/**
+ * @Description:
+ * @Auther: Jack You
+ * @Date: 2022/07/01/10:06
+ */
+public abstract class Test20220701a {
+
+    private static final String a = "a";
+
+     abstract void test();
+}
